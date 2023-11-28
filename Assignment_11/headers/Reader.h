0f1026e5286948a073b4adbd9b495ba10aec0d01
adesh6601 <149320733+adesh6601@users.noreply.github.com>
2023-11-28 23:41:54 +0530

    Add files via upload

---
+++ Assignment_11/headers/Reader.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "Triangle.h"
+
+class Reader
+{
+public:
+    Reader(std::string filePath);
+    ~Reader();
+
+    void getTriangles(std::vector<Triangle> &triangles);
+
+private:
+    std::string mFilePath;
+};
