66e629b4b939db7767621750238f64e4dd694d22
adesh6601 <149320733+adesh6601@users.noreply.github.com>
2023-11-28 23:33:27 +0530

    Add files via upload

---
+++ Assignment_4/main.cpp
@@ -0,0 +1,14 @@
+#include "headers/Reader.h"
+#include "headers/BBox.h"
+#include "headers/Writer.h"
+
+int main()
+{
+    std::vector<Triangle> triangles;
+
+    Reader readShape("resources/cubeModel.stl");
+    readShape.getTriangles(triangles);
+
+    Writer writeGeometry;
+    writeGeometry.write("Shape.txt", triangles);
+}
